7ecf7a772b4260bd295f87e00f93084ac1cbb9d1
Michael Bond <mbond@the-forgotten.org>
2015-06-25 18:11:55 -0400

    base config file, since it no longer gets written by the installer

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$blog_email = "mbond@the-forgotten.org";
+$blog_twitter = "mbond";
+$blog_url = "http://the-forgotten.org:80/";
+$blog_title = "The-Forgotten.Org";
+$meta_description = "";
+$intro_title = "Welcome to The-Forgotten.Org";
+$intro_text = "";
+$header_inject = "";
+$footer_inject = "";
+$template = "simple";
