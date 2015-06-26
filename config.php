f9420bedad84539f8daa217519352e4cf8a31bc8
Michael Bond <mbond@the-forgotten.org>
2015-06-26 07:34:29 -0400

    removed port number from config file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $blog_email = "mbond@the-forgotten.org";
 $blog_twitter = "mbond";
-$blog_url = "http://the-forgotten.org:80/";
+$blog_url = "http://the-forgotten.org/";
 $blog_title = "The-Forgotten.Org";
 $meta_description = "";
 $intro_title = "Welcome to The-Forgotten.Org";
